5cf05019ef6bd012da0fd4be558970ec6f8a9fd1
Dean Howe <hideanhowe@gmail.com>
2025-11-24 21:48:47 +0000

    nearly there

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// Character Count
+// Given a sentence string, return an array with a count of each character in alphabetical order.
+
+// Treat upper and lowercase letters as the same letter when counting.
+// Ignore numbers, spaces, punctuation, etc.
+// Return the count and letter in the format "letter count". For instance, "a 3".
+// All returned letters should be lowercase.
+// Do not return a count of letters that are not in the given string.
+
 function countCharacters(sentence) {
   //console.log(sentence)
   // string to lowercase
@@ -23,6 +32,7 @@ function countCharacters(sentence) {
   // arrange in alphabetical order
   const sorted = filteredArray.sort();
   console.log(sorted);
+  // theres a simple way to get the value and number of instances...
 
   return sentence;
 }
