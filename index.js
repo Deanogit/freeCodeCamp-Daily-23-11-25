ee48b09ca0122de2b44ea5818e8e6283ef214759
Dean Howe <hideanhowe@gmail.com>
2025-11-24 21:18:23 +0000

    testing

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ function countCharacters(sentence) {
   //console.log(lowercase)
   // ignore numbers, spaces, punctuation etc
   const regex = /[a-z]+/;
-  const filtered = lowercase.split(regex);
-  console.log(filtered);
-  // chuck into an array;
-  const array = filtered.map((x) => x.split(''));
+  function filterArray(x) {
+    return regex.test(x);
+  }
+  const array = lowercase.split('');
+  //const array = lowercase.split("");
   console.log(array);
+  const filteredArray = [];
+  const filtered = array.map((x) => {
+    if (regex.test(x)) {
+      filteredArray.push(x);
+    }
+  });
+  // const filtered = array.filter(filterArray(array))
+  // const filtered = array.filter(filter(array))
+  console.log(filteredArray);
+
   return sentence;
 }
+
+// The filter() method creates a new array filled with elements that pass a test provided by a function.
+
+// The filter() method does not execute the function for empty elements.
+
+// The filter() method does not change the original array.
